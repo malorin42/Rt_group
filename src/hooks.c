323a6aaf5ece9bb1d866e3c74a9b7f3966323e39
Jules BAHUS <jbahus@e1r4p12.42.fr>
2017-05-04 17:30:48 +0200

    loading menu

--- src/hooks.c
+++ src/hooks.c
@@ -17,6 +17,7 @@ static void		refresh(t_env *env)
 		i++;
 	}
 	env->menu->render = 1;
+	mlx_string_put(((t_env*)env)->mlx, ((t_env*)env)->win_scene, 100, 100, 0xF00D532, "Loading...");
 }
 
 static void		change_scene(t_env *env)
@@ -121,17 +122,25 @@ int				key_hook(int keycode, t_env *env)
 	return (0);
 }
 
+void			*wait_draw(void *env)
+{
+	mlx_string_put(((t_env*)env)->mlx, ((t_env*)env)->win_scene, 100, 100, 0xF00D532, ((t_env*)env)->win_scene);
+    pthread_exit(0);
+}
+
 int				loop_hook(t_env *env)
 {
 	int 		i;
+	pthread_t   th;
 
-	mlx_clear_window(env->mlx, env->win_menu);
-	mlx_put_image_to_window(env->mlx, env->win_menu, env->menu->img->img, 0, 0);
-	draw_menu(env);
 	if (env->menu->render == 1)
 	{
+		mlx_clear_window(env->mlx, env->win_scene);
+		if (pthread_create(&th, NULL, wait_draw, env) != 0)
+            ft_error("Thread create went wrong.");
 		i = 0;
 		multi_threading(env);
+		pthread_join(th, NULL);
 		while (i < THREAD)
 		{
 			mlx_put_image_to_window(env->mlx, env->win_scene, env->img[i]->img, 0, (HEIGHT / THREAD) * i);
@@ -139,5 +148,8 @@ int				loop_hook(t_env *env)
 		}
 		env->menu->render = 0;
 	}
+	mlx_clear_window(env->mlx, env->win_menu);
+	mlx_put_image_to_window(env->mlx, env->win_menu, env->menu->img->img, 0, 0);
+	draw_menu(env);
 	return (0);
 }
