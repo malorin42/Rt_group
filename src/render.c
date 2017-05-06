a0bbdf16a09527a14fc1514599e5850a7929cf5c
Jules BAHUS <jbahus@e1r2p11.42.fr>
2017-05-06 08:24:38 +0200

    ajout anti-aliasing

--- src/render.c
+++ src/render.c
@@ -16,6 +16,40 @@ static t_double3		pixel_ray_init(t_double3 camera_dir, int x, int y)
 	return (pixel_ray);
 }
 
+static t_double3        pixel_ray_init_aa(t_double3 camera_dir, int x, int y, int i)
+{
+    t_double3   pixel_ray;
+    double      aspect_ratio;
+    double      scale;
+
+    aspect_ratio = (double)WIDTH / (double)HEIGHT;
+    scale = tan((FOV * 0.5 * PI) / 180.0);
+    if (i == 0)
+    {
+        pixel_ray.x = (2 * ((x + 0.25) / (double)WIDTH) - 1) * aspect_ratio * scale;
+        pixel_ray.y = (1 - 2 * (y + 0.25) / (double)HEIGHT) * scale;
+    }
+    else if (i == 1)
+    {
+        pixel_ray.x = (2 * ((x + 0.75) / (double)WIDTH) - 1) * aspect_ratio * scale;
+        pixel_ray.y = (1 - 2 * (y + 0.25) / (double)HEIGHT) * scale;
+    }
+    else if (i == 2)
+    {
+        pixel_ray.x = (2 * ((x + 0.25) / (double)WIDTH) - 1) * aspect_ratio * scale;
+        pixel_ray.y = (1 - 2 * (y + 0.75) / (double)HEIGHT) * scale;
+    }
+    else if (i == 3)
+    {
+        pixel_ray.x = (2 * ((x + 0.75) / (double)WIDTH) - 1) * aspect_ratio * scale;
+        pixel_ray.y = (1 - 2 * (y + 0.75) / (double)HEIGHT) * scale;
+    }
+    pixel_ray.z = -1;
+    pixel_ray = normalize(pixel_ray);
+    pixel_ray = rotation(pixel_ray, camera_dir, REGULAR_MATRIX);
+    return (pixel_ray);
+}
+
 static void            init_de_merde(void *env, int *index)
 {
     pthread_mutex_lock(&((t_env*)env)->my_mutex);
@@ -26,11 +60,12 @@ static void            init_de_merde(void *env, int *index)
 
 void            *render(void *env)
 {
-    t_double3   pixel_ray;
+    t_double3   pixel_ray[4];
     t_double3   color;
     int         x;
     int         y;
     int         index;
+    int         i;
  
     init_de_merde(env, &index);
     y = 0;
@@ -39,11 +74,34 @@ void            *render(void *env)
         x = 0;
         while (x < WIDTH)
         {
-            pixel_ray = pixel_ray_init(((t_env*)env)->scene->camera.dir, x,
+             i = 0;
+            if (((t_env*)env)->scene->aliaising)
+            {
+                while(i < 4)
+                {
+                    pixel_ray[i] = pixel_ray_init_aa(((t_env*)env)->scene->camera.dir, x,
+                    y + ((HEIGHT / THREAD) * (index)), i);
+                    color = v_plus_v(color, raytracer((t_vector){((t_env*)env)->scene->camera.pos, 
+                    pixel_ray[i]}, ((t_env*)env)->scene, NULL, 0));
+                    color = scale_v(color, 0.50);
+                    i++;
+                }
+            }
+            else
+            {
+                pixel_ray[0] = pixel_ray_init(((t_env*)env)->scene->camera.dir, x,
                 y + ((HEIGHT / THREAD) * (index)));
-            color = raytracer((t_vector){((t_env*)env)->scene->camera.pos, 
-                pixel_ray}, ((t_env*)env)->scene, NULL, 0);
-            color_standard((t_env*)env, color, x, y, index);
+                color = raytracer((t_vector){((t_env*)env)->scene->camera.pos, 
+                pixel_ray[0]}, ((t_env*)env)->scene, NULL, 0);
+            }
+            if (((t_env*)env)->scene->effect == 1)
+                color_sepia((t_env*)env, color, x, y, index);
+            else if (((t_env*)env)->scene->effect == 3)
+                color_sepia_neg((t_env*)env, color, x, y, index);
+            else if (((t_env*)env)->scene->effect == 3)
+                color_neg((t_env*)env, color, x, y, index);
+            else
+                color_standard((t_env*)env, color, x, y, index);
             x++;
         }
         y++;
