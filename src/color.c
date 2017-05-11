3f93e91694a2aa79ded3cb972630ba29fa647ef1
Jules BAHUS <jbahus@e1r9p8.42.fr>
2017-05-11 21:00:46 +0200

    reglage saturation

--- src/color.c
+++ src/color.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   color.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: malorin <malorin@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jbahus <jbahus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/09 12:42:18 by jbahus            #+#    #+#             */
-/*   Updated: 2017/05/11 20:04:52 by malorin          ###   ########.fr       */
+/*   Updated: 2017/05/11 21:00:20 by jbahus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,9 +54,9 @@ void			color_standard(t_env *env, t_double3 color, int *xy, int index)
 		g = 255 - g;
 		b = 255 - b;
 	}
-	rgb_color.b.r = r;
-	rgb_color.b.g = g;
-	rgb_color.b.b = b;
+	rgb_color.b.r = max_double(0, min_double(255, r));
+	rgb_color.b.g = max_double(0, min_double(255, g));
+	rgb_color.b.b = max_double(0, min_double(255, b));
 	color_pixel_image(rgb_color, (WIDTH * xy[1] + xy[0]) * env->img[index]->opp,
 		env->img[index]);
 }
