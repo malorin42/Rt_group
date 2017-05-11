f9f3b8687c6f0eb72ec9ebe51f7cabe64e8312b9
Jules BAHUS <jbahus@e1r9p8.42.fr>
2017-05-11 19:35:56 +0200

    reglage pb anti-aliasing

--- src/anti_aliasing.c
+++ src/anti_aliasing.c
@@ -6,62 +6,42 @@
 /*   By: jbahus <jbahus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/11 14:09:07 by jbahus            #+#    #+#             */
-/*   Updated: 2017/05/11 14:36:40 by jbahus           ###   ########.fr       */
+/*   Updated: 2017/05/11 19:31:38 by jbahus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../rtv1.h"
 
-static t_double3		pixel_ray_init_aa_2(t_double3 camera_dir,
-	int x, int y, int i)
+static void				pixel_ray_init_aa_2(t_double3 *pixel_ray,
+	double x, double y)
 {
-	t_double3	pixel_ray;
 	double		aspect_ratio;
 	double		scale;
 
 	aspect_ratio = (double)WIDTH / (double)HEIGHT;
 	scale = tan((FOV * 0.5 * PI) / 180.0);
-	if (i == 2)
-	{
-		pixel_ray.x = (2 * ((x + 0.25) / (double)WIDTH) - 1) *
-		aspect_ratio * scale;
-		pixel_ray.y = (1 - 2 * (y + 0.75) / (double)HEIGHT) * scale;
-	}
-	else if (i == 3)
-	{
-		pixel_ray.x = (2 * ((x + 0.75) / (double)WIDTH) - 1) *
-		aspect_ratio * scale;
-		pixel_ray.y = (1 - 2 * (y + 0.75) / (double)HEIGHT) * scale;
-	}
-	pixel_ray.z = -1;
-	pixel_ray = normalize(pixel_ray);
-	pixel_ray = rotation(pixel_ray, camera_dir, REGULAR_MATRIX);
-	return (pixel_ray);
+	pixel_ray->x = (2 * ((x) / (double)WIDTH) - 1) *
+	aspect_ratio * scale;
+	pixel_ray->y = (1 - 2 * (y) / (double)HEIGHT) * scale;
 }
 
 static t_double3		pixel_ray_init_aa_1(t_double3 camera_dir,
 	int x, int y, int i)
 {
 	t_double3	pixel_ray;
-	double		aspect_ratio;
-	double		scale;
 
-	aspect_ratio = (double)WIDTH / (double)HEIGHT;
-	scale = tan((FOV * 0.5 * PI) / 180.0);
 	if (i == 0)
-	{
-		pixel_ray.x = (2 * ((x + 0.25) / (double)WIDTH) - 1) *
-		aspect_ratio * scale;
-		pixel_ray.y = (1 - 2 * (y + 0.25) / (double)HEIGHT) * scale;
-	}
+		pixel_ray_init_aa_2(&pixel_ray, x + 0.25, y + 0.25);
 	else if (i == 1)
-	{
-		pixel_ray.x = (2 * ((x + 0.75) / (double)WIDTH) - 1) *
-		aspect_ratio * scale;
-		pixel_ray.y = (1 - 2 * (y + 0.25) / (double)HEIGHT) * scale;
-	}
-	pixel_ray = pixel_ray_init_aa_2(camera_dir, x, y, i);
-	return (pixel_ray);
+		pixel_ray_init_aa_2(&pixel_ray, x + 0.75, y + 0.25);
+	else if (i == 2)
+		pixel_ray_init_aa_2(&pixel_ray, x + 0.25, y + 0.75);
+	else if (i == 3)
+		pixel_ray_init_aa_2(&pixel_ray, x + 0.75, y + 0.75);
+	pixel_ray.z = -1;
+	pixel_ray = normalize(pixel_ray);
+	pixel_ray = rotation(pixel_ray, camera_dir, REGULAR_MATRIX);
+	return ((t_double3)pixel_ray);
 }
 
 t_double3				aa(t_env *env, t_double3 color, int *xy, int *i)
